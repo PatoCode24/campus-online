82db6707ce25971cb1cabdcca808350364400901
Patricia Atanasoff <system.informatica365@gmail.com>
2023-05-05 02:09:21 -0300

    swiper js, logo footer

---
+++ js/swiper.js
@@ -0,0 +1,19 @@
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    // Media queries >= 600px
+    breakpoints: {
+        900: {
+            slidesPerView: 2,
+        }
+    }
+});
